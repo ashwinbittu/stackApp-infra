89a06d7f48c28b8d9c4ad21d667346cc513f6d2c
Ashwin Radhakrishnan <ashwin.bittu@gmail.com>
2022-08-23 12:52:52 +1000

    Add files via upload

--- network/main.tf
+++ network/main.tf
@@ -1,8 +1,5 @@
 provider "aws" {
-  #version = "~> 2.28"  
   #region     = var.aws_region
-  #access_key = var.aws_key
-  #secret_key = var.aws_sec
 }
 
 #data "aws_route53_zone" "selected" {
@@ -11,7 +8,7 @@ provider "aws" {
 #}
 
 module "vpc" {
-  source = "app.terraform.io/CentenePoC/vpc/aws"
+  source = "app.terraform.io/radammcorp/vpc/aws"
   #aws_region = var.aws_region
   no_of_subnets = var.no_of_subnets
   aws_vpc_cidr_block   = var.aws_vpc_cidr_block
@@ -21,29 +18,107 @@ module "vpc" {
   aws_vpc_instance_tenancy = var.aws_vpc_instance_tenancy
 }
 
-module "sg" {
-  source = "app.terraform.io/CentenePoC/sg/aws"
+module "sg-elb" {
+  source = "app.terraform.io/radammcorp/sg/aws"
   #aws_region = var.aws_region
   aws_vpc_id = module.vpc.aws_vpc_id
-  app_env   = var.app_env
-  app_name   = var.app_name  
-  app_id   = var.app_id
-  aws_sg_name   = var.aws_sg_name    
+  aws_sg_name   = var.aws_sg_name
+  aws_sg_description = "security group for load balancer"
+
+  ingress_with_cidr_blocks = [
+    {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    },    
+  ]
+
 }
 
-module "elb" {
-  source = "app.terraform.io/CentenePoC/elb/aws"
+module "sg-app" {
+    source = "app.terraform.io/radammcorp/sg/aws"
+    #aws_region = var.aws_region
+    aws_vpc_id = module.vpc.aws_vpc_id
+    aws_sg_name   = var.aws_sg_name   
+    aws_sg_description = "security group for application"
+    ingress_with_cidr_blocks = [
+        {
+            from_port   = 8080
+            to_port     = 8080
+            protocol    = "tcp"
+            security_groups = [module.sg-elb.aws_security_group_instances_id]
+        },    
+      ]       
+  }
+
+module "sg-db" {
+  source = "app.terraform.io/radammcorp/sg/aws"
   #aws_region = var.aws_region
-  aws_subnet_ids = module.vpc.aws_subnet_ids 
-  aws_security_group_elb_id = module.sg.aws_security_group_elb_id
-  lb_ssl_id = "1234"  
-  app_env   = var.app_env
-  app_name   = var.app_name  
-  app_id   = var.app_id    
-}
+  aws_vpc_id = module.vpc.aws_vpc_id
+  aws_sg_name   = var.aws_sg_name  
+  aws_sg_description = "security group for db"
+  ingress_with_cidr_blocks = [
+      {
+          from_port   = 3306
+          to_port     = 3306
+          protocol    = "tcp"
+          security_groups = [module.sg-app.aws_security_group_instances_id]
+      },    
+    ]    
+}  
+
+module "sg-cache" {
+    source = "app.terraform.io/radammcorp/sg/aws"
+    #aws_region = var.aws_region
+    aws_vpc_id = module.vpc.aws_vpc_id
+    aws_sg_name   = var.aws_sg_name    
+    aws_sg_description = "security group for cache"
+    ingress_with_cidr_blocks = [
+        {
+            from_port   = 11211
+            to_port     = 11211
+            protocol    = "tcp"
+            security_groups = [module.sg-app.aws_security_group_instances_id]
+        },    
+      ]     
+} 
+
+module "sg-message" {
+    source = "app.terraform.io/radammcorp/sg/aws"
+    #aws_region = var.aws_region
+    aws_vpc_id = module.vpc.aws_vpc_id
+    aws_sg_name   = var.aws_sg_name    
+    aws_sg_description = "security group for message"
+    ingress_with_cidr_blocks = [
+        {
+            from_port   = 5672
+            to_port     = 5672
+            protocol    = "tcp"
+            security_groups = [module.sg-app.aws_security_group_instances_id]
+        },    
+      ]     
+} 
+
+#module "elb" {
+#  source = "app.terraform.io/radammcorp/elb/aws"
+  #aws_region = var.aws_region
+#  aws_subnet_ids = module.vpc.aws_subnet_ids 
+#  aws_security_group_elb_id = module.sg.aws_security_group_elb_id
+#  lb_ssl_id = "1234"  
+#  app_env   = var.app_env
+#  app_name   = var.app_name  
+#  app_id   = var.app_id    
+#}
 
 #module "route53" {
-#  source = "app.terraform.io/CentenePoC/route53/aws"
+#  source = "app.terraform.io/radammcorp/route53/aws"
 #  aws_region = var.aws_region
 #  app_env   = var.app_env
 #  app_name  = var.app_name  
@@ -65,15 +140,11 @@ module "elb" {
 #  aws_vpc_id = module.vpc.aws_vpc_id
 #}
 
-module "ec2key" {
-  source = "app.terraform.io/CentenePoC/ec2-key/aws"
+#module "ec2key" {
+#  source = "app.terraform.io/radammcorp/ec2-key/aws"
   #aws_region = var.aws_region
-  app_env   = var.app_env
-  app_name   = var.app_name  
-  app_id   = var.app_id 
-  path_to_public_key = var.path_to_public_key   
-}
-
-
-
-
+#  app_env   = var.app_env
+#  app_name   = var.app_name  
+#  app_id   = var.app_id 
+#  path_to_public_key = var.path_to_public_key   
+#}
