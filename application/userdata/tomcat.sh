2bb48dbed869910c95a53bcdbf4ffea41563023e
Ashwin Radhakrishnan <ashwin.bittu@gmail.com>
2022-08-26 09:27:18 +1000

    Add files via upload

--- application/userdata/tomcat.sh
+++ application/userdata/tomcat.sh
@@ -1,9 +1,11 @@
 export DEBIAN_FRONTEND="noninteractive"
 echo set debconf to Noninteractive
 echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
+
 sudo apt update -y  > /tmp/user-data.log
 sudo apt install default-jre -y >>  /tmp/user-data.log
 sudo apt install default-jdk -y >> /tmp/user-data.log
 sudo apt install tomcat9 tomcat9-admin tomcat9-docs tomcat9-common git -y >> /tmp/user-data.log
 touch /home/ubuntu/user-data.log
 cat /tmp/user-data.log > /home/ubuntu/user-data.log
+
