54360b4baa0def4d0e243fcb0e36b9463069aad2
Ashwin Radhakrishnan <ashwin.bittu@gmail.com>
2022-08-25 20:06:43 +1000

    Add files via upload

--- application/userdata/tomcat.sh
+++ application/userdata/tomcat.sh
@@ -1,3 +1,6 @@
+export DEBIAN_FRONTEND="noninteractive"
+echo set debconf to Noninteractive
+echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
 sudo apt update -y  > /tmp/user-data.log
 sudo apt install default-jre -y >>  /tmp/user-data.log
 sudo apt install default-jdk -y >> /tmp/user-data.log
