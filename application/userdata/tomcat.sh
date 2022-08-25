41298c10d14af0b72260c42e53a8548c0f28fa2b
Ashwin Radhakrishnan <ashwin.bittu@gmail.com>
2022-08-25 19:36:24 +1000

    Add files via upload

--- application/userdata/tomcat.sh
+++ application/userdata/tomcat.sh
@@ -1,4 +1,6 @@
-sudo apt update
-sudo apt install default-jre -y
-sudo apt install default-jdk -y
-sudo apt install tomcat9 tomcat9-admin tomcat9-docs tomcat9-common git -y
+sudo apt update -y  > /tmp/user-data.log
+sudo apt install default-jre -y >>  /tmp/user-data.log
+sudo apt install default-jdk -y >> /tmp/user-data.log
+sudo apt install tomcat9 tomcat9-admin tomcat9-docs tomcat9-common git -y >> /tmp/user-data.log
+touch /home/ubuntu/user-data.log
+cat /tmp/user-data.log > /home/ubuntu/user-data.log
